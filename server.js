fe438b36a519f81f7eef61cd1977f4a801a3b0b3
Bruce Conrad <bruce_conrad@byu.edu>
2023-06-15 14:17:10 -0600

    conform to instance

--- server.js
+++ server.js
@@ -2,17 +2,32 @@ const express = require('express');
 const app = express();
 
 const DOMAIN = "picolabs";
-const PICO_ENGINE_HOST = "http://localhost:8080";
-const REGISTRY_PICO_ECI = "EH3vvJjseo2BDoVayYA1MF";
+const PICO_ENGINE_HOST = "https://manifold.picolabs.io:9090";
+const REGISTRY_PICO_ECI = "CEmo7mURALxUzEVLkN2Fwc";
+const REDIRECT_URL = "https://manifold.picolabs.io/#/picolabs/tag";
 
-app.get('/:tagID', function (req, res) {
+app.get('/picolabs/:tagID', function (req, res) {
+  const tagID = req.params.tagID;
+  if(!tagID) {
+      res.redirect(REDIRECT_URL);
+    }
+  else {
+    res.redirect(`${PICO_ENGINE_HOST}/sky/event/${REGISTRY_PICO_ECI}/none/safeandmine/redirect?tagID=${tagID}&domain=${DOMAIN}`);
+  }
+});
+
+app.get('/sqtg/:tagID', function (req, res) {
   const tagID = req.params.tagID;
   if(!tagID) {
       res.status(400).send("Missing tagID!");
     }
   else {
-    res.redirect(`${PICO_ENGINE_HOST}/sky/event/${REGISTRY_PICO_ECI}/none/safeandmine/redirect?tagID=${tagID}&domain=${DOMAIN}`);
+    res.redirect(`${PICO_ENGINE_HOST}/sky/event/${REGISTRY_PICO_ECI}/none/safeandmine/redirect?tagID=${tagID}&domain=sqtg`);
   }
+});
+
+app.get('/picolabs/', function (req, res) {
+  res.redirect(REDIRECT_URL);
 });
 
 app.listen(process.env.PORT || 3001);
